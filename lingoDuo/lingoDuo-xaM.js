984f4f84da4a3bda6494b55d667ec1da9c350bb6
Lu Zhuo <125237595+TAKAGIVEGETA@users.noreply.github.com>
2026-01-16 14:06:48 +0800

    Improve validation
    Refactor response handling to skip processing when conditions are not met.

--- lingoDuo/lingoDuo-xaM.js
+++ lingoDuo/lingoDuo-xaM.js
@@ -1,6 +1,7 @@
 try {
     let obj = JSON.parse($response.body);
-    if (obj.responses && obj.responses.length >= 2 && !('etag' in obj.responses[0].headers)) {
+    if (!obj.responses || obj.responses.length < 2 || (obj.responses[0].headers && 'etag' in obj.responses[0].headers)); // skip
+    else {
         const now = Math.floor(Date.now() / 1000);
         const userdata = JSON.parse(obj.responses[0].body);
         if (!userdata.shopItems) userdata.shopItems = [];
@@ -26,7 +27,7 @@ try {
         userdata.trackingProperties.has_item_max_subscription = true;
         obj.responses[0].body = JSON.stringify(userdata);
     }
-    $done({ body: JSON.stringify(obj) })
+    $done({ body: JSON.stringify(obj) });
 } catch (e) {
     $done({});
 }
